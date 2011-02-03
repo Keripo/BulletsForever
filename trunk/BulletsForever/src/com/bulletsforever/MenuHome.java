a85a4b2934506136b68205af2913a64237ba83e2
keripo <keripo@1d7018e3-3fad-4468-9bc6-eee62a915466>
2011-02-03 04:42:20 +0000

    fail?

---
+++ trunk/BulletsForever/src/com/bulletsforever/MenuHome.java
@@ -0,0 +1,13 @@
+package com.bulletsforever;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuHome extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
