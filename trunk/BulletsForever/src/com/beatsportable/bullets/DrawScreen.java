58dbb755e8ef6b2be10e267f6f908c4bb5bebdac
keripo <keripo@1d7018e3-3fad-4468-9bc6-eee62a915466>
2011-03-02 03:19:40 +0000

    - no more OpenGL - I don't want to learn it - if anyone wants and drawing speed turns out to be a problem later on, feel free to learn it and finish this wrapper

--- trunk/BulletsForever/src/com/beatsportable/bullets/DrawScreen.java
+++ trunk/BulletsForever/src/com/beatsportable/bullets/DrawScreen.java
@@ -1,22 +1,33 @@
 package com.beatsportable.bullets;
 
+import java.io.IOException;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 import android.view.MotionEvent;
 
 /**
+ * OpenGL is too complicated
+ * I am a software engineer, not graphics programmer.
+ * I don't want to bother learning this from scratch if no one else bothers even trying.
+ * ~Phil, 2011/03/01
+ * 
  * Wrapper for our OpenGL View, with extras
  */
 
+@Deprecated
 public class DrawScreen {
 
+	private Context c;
 	private CustomGLView mGLView;
 	
 	// Setup mGLView and mRenderer
 	public DrawScreen(Context c) {
+		this.c = c;
 		mGLView = new CustomGLView(c);
 	}
 	
@@ -51,6 +62,11 @@ public class DrawScreen {
 				public void run() {
 					// Do something here
 					//mRenderer.doSomething(event.getX(), event.getY());
+					
+					// TODO - For testing purposes
+					if (event.getAction() == MotionEvent.ACTION_DOWN) {
+						mRenderer.randomizeBackgroundColour();
+					}
 				}});
 			return true;
 		}
@@ -60,16 +76,37 @@ public class DrawScreen {
 	// Custom Renderer for our mGLView
 	class CustomRenderer implements GLSurfaceView.Renderer
 	{
-
 		/*
-		 * This is the main "update frame" method! See javadocs
+		 * This is for FPS testing
+		 */
+		private boolean showFPS;
+		private DrawFPSCounter fpsCounter;
+		//private DrawTextbox fpsTextbox;
+		
+		/*
+		 * Do initial setup stuff here, see javadocs
 		 */
 		@Override
-		public void onDrawFrame(GL10 gl) {
-			// Clear screen
-			gl.glClearColor(0f, 0f, 0f, 1.0f); // black
-			gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-	        gl.glTexEnvx(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE, GL10.GL_REPLACE);
+		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+			
+			// Some OpenGL setup stuff that I copy-pasted
+			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+			gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+			gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
+			gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+			
+			// TODO - For testing purposes
+			randomizeBackgroundColour();
+			//try {
+				showFPS = true; // Disable in final version?
+				fpsCounter = new DrawFPSCounter();
+				//fpsTextbox = new DrawTextbox(DrawScreen.this.c, gl);
+				//fpsTextbox.LoadFontAlt("fonts/HappyKiller.ttf", gl);
+			//} catch (IOException e) {
+				// blah
+				//showFPS = false;
+				//Log.e("BLAHHHH", e.getMessage());
+			//}
 			
 		}
 		
@@ -80,18 +117,36 @@ public class DrawScreen {
 		public void onSurfaceChanged(GL10 gl, int w, int h) {
 			gl.glViewport(0, 0, w, h);			
 		}
-
+		
+		
+		// TODO - For testing purposes
+		private float bgR, bgG, bgB;
+		public void randomizeBackgroundColour() {
+			bgR = (float)Math.random();
+			bgG = (float)Math.random();
+			bgB = (float)Math.random();
+		}
+		
 		/*
-		 * Do initial setup stuff here, see javadocs
+		 * This is the main "update frame" method! See javadocs
 		 */
 		@Override
-		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		public void onDrawFrame(GL10 gl) {
 			
-			// Some OpenGL setup stuff that I copy-pasted
-			gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-	        gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-	        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
-	        gl.glTexParameterx(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
+			// Clear screen
+			//gl.glClearColor(0f, 0f, 0f, 1.0f); // black
+			gl.glClearColor(bgR, bgG, bgB, 1.0f); // TODO - For testing purposes
+			gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+	        gl.glTexEnvx(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE, GL10.GL_REPLACE);
+			
+
+			// Show FPS counter
+			if (showFPS) {
+				fpsCounter.nextFrame();
+				
+				// Draw values
+				//fpsTextbox.PrintAt(gl, fpsCounter.getDisplayedFPS(), 50, 50);
+			}
 			
 		}
 		
