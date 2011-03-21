5872d9e96fe0c8a11da5140d87a3aec86a1da5c2
Keripo <k.darktiger@gmail.com>
2011-03-21 22:49:43 +0000

    - fixed max player movement speed (via ratio capping) - cleaned up commented code in DrawObjectPlayer.java

--- BulletsForever/src/com/bulletsforever/bullets/DrawObjectPlayer.java
+++ BulletsForever/src/com/bulletsforever/bullets/DrawObjectPlayer.java
@@ -9,8 +9,9 @@ import android.graphics.Canvas;
  */
 public class DrawObjectPlayer extends DrawObject {
 
-	//private Paint avatarPaint;
 	public float tx, ty;
+	private static final float MAX_SPEED = 20f;
+	private static final float DAMP_RATIO = 0.75f;
 	
 	public DrawObjectPlayer(DrawWorld dw) {
 		// Middle of the screen
@@ -24,63 +25,35 @@ public class DrawObjectPlayer extends DrawObject {
 		bitmap = dw.bl.getBitmap(R.drawable.iconplayer, hitboxHalfWidth, hitboxHalfHeight);
 		tx = x; // Don't move
 		ty = y; // Don't move
-		//avatarPaint = new Paint();
-		//avatarPaint.setColor(Color.BLUE);
 	}
 	
 	public void nextFrame() {
 		// Move to where the target destination is
-		float dtx = (tx > x) ? (tx - x) : (x - tx);
-		float dty = (ty > y) ? (ty - y) : (y - ty);
-		if (dtx > 0.5f && dty > 0.5f) { // floating point precision of 0.5 pixel (possible?)
-			dx = (tx - x) * 0.1f;
-			dy = (ty - y) * 0.1f;
+		float dtx = ((tx > x) ? (tx - x) : (x - tx)) * DAMP_RATIO;
+		float dty = ((ty > y) ? (ty - y) : (y - ty)) * DAMP_RATIO;
+		if (dtx > 0.5f || dty > 0.5f) { // floating point precision of 0.5 pixel (possible?)
+			dx = (tx - x) * DAMP_RATIO;
+			dy = (ty - y) * DAMP_RATIO;
+			if (dtx > MAX_SPEED || dty > MAX_SPEED) {
+				float ratio = MAX_SPEED / (float)Math.sqrt(dx * dx + dy * dy);
+				dx *= ratio;
+				dy *= ratio;
+			}
 			super.nextFrame();
+		} else {
+			dx = 0;
+			dy = 0;
 		}
-		//super.nextFrame();
-		
-		// Lalalala
-		// This player is hyper and running off coffee, like me! (s/coffee/tea)
-		// ~Phil, 4am in the morning
-		
-		
-		// the following lines have been commented by Austin and Yash on 17th march night.
-		/*
-		x += (Math.random() - 0.5f) * 15;
-		y += (Math.random() - 0.5f) * 15;
-		if (x < hitboxHalfWidth ||
-			y < hitboxHalfHeight ||
-			x > Settings.screenWidth - hitboxHalfWidth ||
-			y > Settings.screenHeight - hitboxHalfHeight
-			){ // Offscreen
-			x = ((float)Math.random() * (Settings.screenWidth - hitboxHalfWidth)) + hitboxHalfWidth;
-			y = ((float)Math.random() * (Settings.screenHeight - hitboxHalfHeight)) + hitboxHalfHeight;
-		}
-		*/
 	}
 	
 	@Override
 	public void draw(Canvas canvas) {
-		/*
-		canvas.drawRect(
-				x - hitboxHalfWidth, y - hitboxHalfHeight,
-				x + hitboxHalfWidth, y + hitboxHalfHeight,
-				avatarPaint
-				);
-		*/
 		canvas.drawBitmap(bitmap, x - hitboxHalfWidth, y - hitboxHalfHeight, null);
 	}
 
 	@Override
 	public void onCollision(DrawObject object) {
-		/*
-		avatarPaint.setARGB(
-				255,
-				(int)(Math.random() * 255),
-				(int)(Math.random() * 255),
-				(int)(Math.random() * 255)
-				);
-		*/
+		// TODO
 	}
 
 }
