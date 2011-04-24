4263bde584ad65216dda14b95c7e2c8230a39d72
sandanzuki <sandanzuki@1d7018e3-3fad-4468-9bc6-eee62a915466>
2011-04-24 22:43:04 +0000

    Made boss "cooler": 1) Separate left and right arm evolution 2) Left and right arms branch down to form trident-shaped boss (need to check positioning on this) 3) Fully-grown arms shoot patterned bullets 4) Destroyed arm parts release suicide bullets Can't test on this computer, so as yet undebugged.

--- trunk/BulletsForever/src/com/bulletsforever/bullets/DrawObjectDynamicArm.java
+++ trunk/BulletsForever/src/com/bulletsforever/bullets/DrawObjectDynamicArm.java
@@ -28,34 +28,46 @@ public class DrawObjectDynamicArm extends DrawObject {
 		this.dw = dw;
 	}
 	
-	public void nextFrame(boolean fire) {
+	public void nextFrame(boolean fire, boolean maxed) {
 		super.nextFrame();
 
 		this.x = this.core.x + this.displacex;
 		this.y = this.core.y + this.displacey;
 		if (fire)
-			this.fire();
+			this.fire(maxed);
 	}	
 	
-	private void fire() {
+	private void fire(boolean maxed) {
 		
-		// Calculate angle between this and player
-		float dx = dw.player.x - this.x;
-		float dy = Math.abs(dw.player.y - this.y);
-		float angle;
-		if (dy != 0)
-			angle = (float)(Math.atan(dx/dy) * 180f / (float)Math.PI); 
-		else {
-			if (dx > 0) angle = 90;
-			else if (dx == 0) angle = 0;
-			else angle = 270;
+		if(!maxed) {
+			// Calculate angle between this and player
+			float dx = dw.player.x - this.x;
+			float dy = Math.abs(dw.player.y - this.y);
+			float angle;
+			if (dy != 0)
+				angle = (float)(Math.atan(dx/dy) * 180f / (float)Math.PI); 
+			else {
+				if (dx > 0) angle = 90;
+				else if (dx == 0) angle = 0;
+				else angle = 270;
+			}
+		
+			dw.addBullet(new DrawObjectBullet(dw, true, 
+					this.x, this.y, 
+					4f, 0f, 0f, 0f, angle, 0f
+					));
 		}
-		
-		dw.addBullet(new DrawObjectBullet(dw, 
-				true, 
-				this.x, this.y, 
-				4f, 0f, 0f, 0f, angle, 0f
-				));
+		else {
+			// Max powered arms shoot ring-pattern bullets
+			for (int i = 0; i < 360; i += 10) {
+				DrawObjectBullet bullet = new DrawObjectBullet(
+						dw, true,
+						this.x, this.y,
+						5f, 0f, 0f, 0f, i, 10f
+						);
+				dw.addBullet(bullet);
+			}
+		}
 	}
 	
 	public void draw(Canvas canvas) {
@@ -66,9 +78,19 @@ public class DrawObjectDynamicArm extends DrawObject {
 		this.health--;
 	}
 
-	public int numChildren() {
-		if (this.child == null)
+	/* Returns number of children destroyed */
+	public int destroyChildren() {
+		// suicide bullets in zoom-pattern
+		for (int i = 0; i < 360; i += 10) {
+			DrawObjectBullet bullet = new DrawObjectBullet(
+					dw, true,
+					this.x, this.y,
+					0f, 0.5f, 0f, 0f, i, -15f
+					);
+			dw.addBullet(bullet);
+		}
+		if (this.child == null) 
 			return 0;
-		return this.child.numChildren() + 1;
+		return this.child.destroyChildren() + 1;
 	}
 }
